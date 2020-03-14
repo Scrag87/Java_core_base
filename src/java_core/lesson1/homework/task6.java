70042a53549aa2042cf345d3d238a709cddfe1c3
scrag <scrag1987@gmail.com>
2020-03-14 16:32:27 +0600

    task6 done

--- src/java_core/lesson1/homework/task6.java
+++ src/java_core/lesson1/homework/task6.java
@@ -2,7 +2,7 @@ package java_core.lesson1.homework;
 
 public class task6 {
     public static void main(String[] args) {
-        System.out.println(isTrue(-1));
+        System.out.println(isTrue(-20));
     }
     static boolean isTrue(int number){
     return number<0;
