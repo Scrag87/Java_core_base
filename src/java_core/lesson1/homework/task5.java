aa952d555542162004c7585077f6d925ddb9cd9a
scrag <scrag1987@gmail.com>
2020-03-14 16:27:49 +0600

    task5 done

---
+++ src/java_core/lesson1/homework/task5.java
@@ -0,0 +1,11 @@
+package java_core.lesson1.homework;
+
+public class task5 {
+    public static void main(String[] args) {
+        positiveOrNegative(-1);
+    }
+    static void positiveOrNegative(int number){
+        if (number>=0) System.out.println("Number is positive");
+        else System.out.println("Number is negative");
+    }
+}
