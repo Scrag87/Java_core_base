fcd19b4f187a7c5026254cb9b54753c522681896
scrag <scrag1987@gmail.com>
2020-03-14 16:40:36 +0600

    task7 done

--- src/java_core/lesson1/homework/task7.java
+++ src/java_core/lesson1/homework/task7.java
@@ -2,10 +2,10 @@ package java_core.lesson1.homework;
 
 public class task7 {
     public static void main(String[] args) {
-Greeting("Ana");
+        Greeting("Ana");
     }
 
     static void Greeting(String name) {
-        System.out.println("Welcome, " + name+"!");
+        System.out.println("Welcome, " + name + "!");
     }
 }
